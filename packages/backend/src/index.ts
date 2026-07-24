27aeba6a555a5e1676c84bea2800931e9bf62d3c
cittan <3198001545@qq.com>
2026-07-24 18:00:36 +0800

    配置后端跨域请求

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
 import { z } from "zod"
 import { nanoid } from "nanoid"
+import { cors } from 'hono/cors'
+
 
 type Env = {
    Bindings: {
@@ -10,6 +12,10 @@ type Env = {
 }
 
 const app = new Hono<Env>();
+
+//配置跨域请求
+app.use('*',cors())
+
 app.get('/', (c) => {
    return c.text('Hello World!')
 })
@@ -19,28 +25,28 @@ app.get('/api/health', (c) => {
 })
 
 const shortenSchema = z.object({
-   url: z.string().url({message: "必须是有效的 URL"}),
+   url: z.string().url({ message: "必须是有效的 URL" }),
 })
 
 app.post('/api/shorten', async (c) => {
    const body = await c.req.json();
    const url = shortenSchema.safeParse(body);
-   if(!url.success){
-      return c.json({error: url.error.issues[0]?.message || '请求参数错误'}, 400);
+   if (!url.success) {
+      return c.json({ error: url.error.issues[0]?.message || '请求参数错误' }, 400);
    }
    const validUrl = url.data.url;
    const shortCode = nanoid(6);
    await c.env.KV.put(shortCode, validUrl);
    const shortUrl = `https://${c.env.SHORT_DOMAIN}/${shortCode}`;
-   return c.json({shortUrl});
+   return c.json({ shortUrl });
 
 })
 
 app.get('/:code', async (c) => {
    const code = c.req.param('code');
    const url = await c.env.KV.get(code);
-   if(!url) {
-      return c.text("链接不存在",404);
+   if (!url) {
+      return c.text("链接不存在", 404);
    }
    return c.redirect(url, 302);
 })
