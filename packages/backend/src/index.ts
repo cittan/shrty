36f01f1a6bcb89cd1c4769073d0fa121b7a99f69
cittan <3198001545@qq.com>
2026-07-24 14:42:50 +0800

    完成后端生成短链与重定向

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,12 +1,48 @@
- import { Hono } from 'hono'
+import { Hono } from 'hono'
+import { z } from "zod"
+import { nanoid } from "nanoid"
 
- const app = new Hono();
- app.get('/', (c) => {
-    return c.text('Hello World!')
- })
+type Env = {
+   Bindings: {
+      KV: KVNamespace,
+      SHORT_DOMAIN: string
+   }
+}
 
- app.get('/api/health', (c) => {
-    return c.json({ status: 'healthy' })
- })
+const app = new Hono<Env>();
+app.get('/', (c) => {
+   return c.text('Hello World!')
+})
 
- export default app
+app.get('/api/health', (c) => {
+   return c.json({ status: 'healthy' })
+})
+
+const shortenSchema = z.object({
+   url: z.string().url({message: "必须是有效的 URL"}),
+})
+
+app.post('/api/shorten', async (c) => {
+   const body = await c.req.json();
+   const url = shortenSchema.safeParse(body);
+   if(!url.success){
+      return c.json({error: url.error.issues[0]?.message || '请求参数错误'}, 400);
+   }
+   const validUrl = url.data.url;
+   const shortCode = nanoid(6);
+   await c.env.KV.put(shortCode, validUrl);
+   const shortUrl = `https://${c.env.SHORT_DOMAIN}/${shortCode}`;
+   return c.json({shortUrl});
+
+})
+
+app.get('/:code', async (c) => {
+   const code = c.req.param('code');
+   const url = await c.env.KV.get(code);
+   if(!url) {
+      return c.text("链接不存在",404);
+   }
+   return c.redirect(url, 302);
+})
+
+export default app
